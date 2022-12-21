90e567d0b69232acffcef3426802716b5dfbd442
FarhanaIslamSaima <farhanaislamsaima@gmail.com>
2022-12-22 00:21:21 +0600

    updtaed

--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -5,5 +5,11 @@ const tokens = (n) => {
 }
 
 describe("ETHDaddy", () => {
+  it('has a name',async()=>{
+    const ETHDaddy=await ethers.getContractFactory('ETHDaddy')
+    let ethDaddy=await ETHDaddy.deploy()
+    const result=await ethDaddy.name()
+    expect(result).to.equal('Eth Daddy')
+  })
 
 })
