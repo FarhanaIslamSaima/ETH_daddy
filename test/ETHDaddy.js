20361ca5aa4dbc301b682ab116f2be8383a92385
FarhanaIslamSaima <farhanaislamsaima@gmail.com>
2022-12-23 01:55:15 +0600

    updated

--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -9,8 +9,11 @@ describe("ETHDaddy", () => {
   const name='Eth Daddy'
   const symbol='ETHD'
   beforeEach(async()=>{
+    [deployer,owner]=await ethers.getSigners();
     const ETHDaddy=await ethers.getContractFactory('ETHDaddy')
     ethDaddy=await ETHDaddy.deploy('Eth Daddy','ETHD')
+    const testalist=await ethDaddy.connect(deployer).list('jackEth',tokens(10));
+    await testalist.wait();
   })
   describe('deployment',async()=>{
     it('has a name',async()=>{
@@ -25,6 +28,19 @@ describe("ETHDaddy", () => {
       expect(result).to.equal(symbol)
     })
   })
- 
+  it('sets the owner',async()=>{
+     
+    let result=await ethDaddy.owner()
+   expect(result).to.equal(deployer.address)
+
+})
+ describe('testing list',async()=>{
+  it('setup domain list',async()=>{
+    let domains=await ethDaddy.domains(1);
+    expect(domains.name).to.be.equal('jackEth')
+    expect(domains.cost).to.be.equal(tokens(10))
+    expect(domains.flag).to.be.equal(false)
+  })
+ })
 
 })
