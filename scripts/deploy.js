a9f33b3c7c47cb0dd9084236ccfb4b03ef7025ee
FarhanaIslamSaima <farhanaislamsaima@gmail.com>
2022-12-23 21:21:29 +0600

    updated

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,6 +11,23 @@ const tokens = (n) => {
 }
 
 async function main() {
+  const [deployer]=await ethers.getSigners();
+  const Name='Eth Daddy';
+  const symbol='ETHD';
+  const Ethdaddy=await ethers.getContractFactory('ETHDaddy');
+  const ethDaddy=await Ethdaddy.deploy(Name,symbol);
+  await ethDaddy.deployed();
+  console.log(`{Deployed domain contract address at:${ethDaddy.address}}`)
+
+  const names=['saima.eth','Boni.eth','Nisa.eth','Raba.eth','Mithila.eth','Samiha.eth','Ontora.eth'];
+  const costs=[tokens(10),tokens(20),tokens(30),tokens(40),tokens(50),tokens(60)]
+  for(var i=0;i<6;i++){
+    const transaction=await ethDaddy.connect(deployer).list(names[i],costs[i]);
+    await transaction.wait();
+
+    console.log(names[i]);
+
+  }
 
 }
 
