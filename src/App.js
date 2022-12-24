8ecd0047a677231003d0556d18985ecd31a62ab0
FarhanaIslamSaima <farhanaislamsaima@gmail.com>
2022-12-24 22:46:58 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -11,15 +11,28 @@ import ETHDaddy from './abis/ETHDaddy.json'
 
 // Config
 import config from './config.json';
+import { use } from 'chai'
 
 function App() {
+  const [account,setAccount]=useState(null);
+  const loadBlockChainData=async()=>{
+    const accounts=await window.ethereum.request({'method':'eth_requestAccounts'})
+    const account=ethers.utils.getAddress(accounts[0]);
+    console.log(account);
+    setAccount(account);
 
+  }
+useEffect(()=>{
+  loadBlockChainData();
+},[])
   return (
     <div>
+      <Navigation account={account} setAccount={setAccount}/>
 
       <div className='cards__section'>
 
         <h2 className='cards__title'>Welcome to ETH Daddy</h2>
+        <p>{account}</p>
 
       </div>
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,58 @@
 import { ethers } from 'ethers';
 import logo from '../assets/logo.svg';
+import {AppBar,Toolbar,makeStyles,Box,Typography} from '@material-ui/core'
+const useStyle=makeStyles({
+  header:{
+    background:'lightgrey'
+
+  },
+  container:{
+    display:'flex'
+  },
+  leftContainer:{
+    display:'flex',
+    color:'black'
+  },
+  rightContainer:{
+
+
+  },
+  list:{
+    display:'flex',
+    flexDirection:'row',
+    '&>*':{
+      padding:'0px 10px'
+    }
+
+  },
+  logo:{
+    fontWeight:'bold',
+    marginRight:'10px'
+
+
+  }
+})
 
 const Navigation = ({ account, setAccount }) => {
-
+  const classes=useStyle()
   return (
-    <nav>
+    <AppBar>
+      <Toolbar className={classes.header} >
+        <Box className={classes.container}>
+          <Box className={classes.leftContainer}>
+            <Typography className={classes.logo}>ETH Daddy</Typography>
+            <Box className={classes.list}>
+              <Typography>Domain Names</Typography>
+              <Typography>Website & Hosting</Typography>
+              <Typography>Commerce</Typography>
+              <Typography>Email & Marketing</Typography>
+            </Box>
+          </Box>
+          <Box className={classes.rightContainer}></Box>
 
-    </nav>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
 
