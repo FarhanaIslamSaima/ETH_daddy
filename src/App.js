30dc3a72e5bf6f6b48e83e0de792a534c55a3dc2
FarhanaIslamSaima <farhanaislamsaima@gmail.com>
2022-12-25 14:37:49 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -15,16 +15,7 @@ import { use } from 'chai'
 
 function App() {
   const [account,setAccount]=useState(null);
-  const loadBlockChainData=async()=>{
-    const accounts=await window.ethereum.request({'method':'eth_requestAccounts'})
-    const account=ethers.utils.getAddress(accounts[0]);
-    console.log(account);
-    setAccount(account);
 
-  }
-useEffect(()=>{
-  loadBlockChainData();
-},[])
   return (
     <div>
       <Navigation account={account} setAccount={setAccount}/>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,24 @@
 import { ethers } from 'ethers';
 import logo from '../assets/logo.svg';
-import {AppBar,Toolbar,makeStyles,Box,Typography} from '@material-ui/core'
+import {AppBar,Toolbar,makeStyles,Box,Typography,Button} from '@material-ui/core'
 const useStyle=makeStyles({
   header:{
     background:'lightgrey'
 
   },
   container:{
-    display:'flex'
+    display:'flex',
+    alignItems:'center',
+    width:'100%'
   },
   leftContainer:{
+    width:'80%',
     display:'flex',
     color:'black'
   },
   rightContainer:{
+    width:'20%',
+    marginLeft:'auto'
 
 
   },
@@ -30,11 +35,23 @@ const useStyle=makeStyles({
     marginRight:'10px'
 
 
+  },
+  button:{
+    background:'black',
+    
+    fontSize:'13px'
   }
 })
 
 const Navigation = ({ account, setAccount }) => {
   const classes=useStyle()
+  const handleConnect=async()=>{
+    const accounts=await window.ethereum.request({'method':'eth_requestAccounts'})
+    const account=await ethers.utils.getAddress(accounts[0]);
+    console.log(account)
+    setAccount(account)
+
+  }
   return (
     <AppBar>
       <Toolbar className={classes.header} >
@@ -48,7 +65,13 @@ const Navigation = ({ account, setAccount }) => {
               <Typography>Email & Marketing</Typography>
             </Box>
           </Box>
-          <Box className={classes.rightContainer}></Box>
+          <Box className={classes.rightContainer}>
+            {
+              account? <Button  className={classes.button}variant={'contained'} color={'primary'}>{account.slice(0,6)+'....'+account.slice(38,42)}</Button>:
+              <Button  className={classes.button} variant={'contained'} color={'primary'} onClick={handleConnect}>Connect</Button>
+            }
+          
+          </Box>
 
         </Box>
       </Toolbar>
